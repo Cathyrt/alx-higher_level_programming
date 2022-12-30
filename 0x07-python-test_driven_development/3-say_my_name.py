fcf2e7ddc771e4d1362eb305ba7e0da3ac898570
Cathyrt <cathrinert20@icloud.com>
2022-12-30 05:46:56 -0800

    Updated files

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""String manipulations"""
+"""
+This is the Name module
+This module contains a simple function to print out a first and last name
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a string.
-    Validates string inputs, the display
-    the full name
-
     """
-    if type(first_name) != str:
-        raise TypeError("first name must be a string")
-    if type(last_name) != str:
-        raise TypeError("last name must be a string")
+    Return nothing
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
