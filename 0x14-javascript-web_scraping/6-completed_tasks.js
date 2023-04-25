72f29d430203db60e21f7ba8a2b823e3c83fa34e
Cathyrt <cathrinert20@icloud.com>
2023-04-25 07:01:09 -0700

    Updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,18 +4,17 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  }
-  const json = JSON.parse(body);
-  const finished = {};
-  for (let i = 0; i < json.length; i++) {
-    if (json[i].completed === true) {
-      if (finished[json[i].userId] === undefined) {
-        finished[json[i].userId] = 0;
-      } 
-      finished[json[i].userId]++;
+  if (err == null) {
+    const json = JSON.parse(body);
+    const finished = {};
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (finished[json[i].userId] === undefined) {
+          finished[json[i].userId] = 0;
+	}
+        finished[json[i].userId]++;
       }
     }
     console.log(finished);
+  }
 });
