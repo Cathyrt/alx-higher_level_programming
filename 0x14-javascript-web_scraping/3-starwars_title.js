ed66bd6ac4827bfa637eda2864aa8a359f02174d
Cathyrt <cathrinert20@icloud.com>
2023-04-25 05:43:33 -0700

    Update starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const movieId = process.argv[2];
 
 request('https://swapi-api.alx-tools.com/api/films/'.concat(movieId), function (err, response, body) {
   if (err) {
-    return console.log(err);
+    console.log(err);
   }
-  console.log(JSON.parse(body)['title']);
+  console.log(JSON.parse(body).title);
 });
